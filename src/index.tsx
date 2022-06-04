93e28857207be71ba489b2a0fdeac652873c3aef
EXPORTER1700 <i.stetsenko1998@gmail.com>
2022-06-04 22:17:55 +0300

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from 'react-redux'
+import {store} from "./store";
+
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+    <BrowserRouter>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </BrowserRouter>
+);
