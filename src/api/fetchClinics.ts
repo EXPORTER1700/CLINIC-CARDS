6a1333852fe05ccbf881d9607a471af1c14ab327
EXPORTER1700 <i.stetsenko1998@gmail.com>
2022-06-23 16:02:19 +0300

    add clinic list and patients list

--- src/api/fetchClinics.ts
+++ src/api/fetchClinics.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 import {FetchURL} from "../constants/fetchURL";
 import {makeRequestParams} from "../helpers/makeRequestParams";
+import {IClinic} from "../types/clinic";
 
 export const fetchClinics = async (param: string, values: string[]) => {
-    return await axios.get(FetchURL.CLINICS + makeRequestParams(param, values))
+    return await axios.get<IClinic[]>(FetchURL.CLINICS + makeRequestParams(param, values))
 }
--- src/pages/ListPages/PatientsListPage.tsx
+++ src/pages/ListPages/PatientsListPage.tsx
@@ -3,9 +3,6 @@ import Wrapper from "../../components/Wrapper/Wrapper";
 import PatientsList from "../../components/List/Lists/PatientsList";
 import {useTypedSelector} from "../../hooks/useTypedSelector";
 import CustomTitle from "../../ui/CustomTitle/CustomTitle";
-import {linksEnum} from "../../constants/routes";
-import SortAndAdd from "../../components/SortAndAdd/SortAndAdd";
-import {IPatient} from "../../types/patient";
 
 const PatientsListPage = () => {
     const {clinic} = useTypedSelector(state => state.clinic)
@@ -13,14 +10,9 @@ const PatientsListPage = () => {
     const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
         setSearch(event.target.value)
     }
-    // const searchClinic = useSearch<IPatient>(clinic.patients, ["firstName" , "lastName"], search)
 
     return (
         <Wrapper title="Список пациентов">
-            <SortAndAdd
-                value={search}
-                onChange={handleChange}
-                to={linksEnum.clinic + linksEnum.slash + clinic.id + linksEnum.createPatient}/>
             <PatientsList patients={clinic.patients}/>
             {!clinic.patients.length && <CustomTitle>Пока нет пациентов</CustomTitle>}
         </Wrapper>
